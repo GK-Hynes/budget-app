5f64541adbdc0bfee1ee48fd20d9f68a398a115d
Gerard Hynes <gkhynes@gmail.com>
2021-05-03 22:10:56 +0100

    Start on Category class for budget

--- budget.py
+++ budget.py
@@ -1,7 +1,35 @@
 class Category:
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
 
+    def __repr__(self):
+	    pass
 
+    def deposit(self, amount, description=""):
+        self.ledger.append({"amount": amount, "description": description})
+
+    def withdraw(self, amount, description):
+        if self.check_funds(amount):
+            self.ledger.append({"amount": -amount, "description": description})
+            return True
+        return False
+
+    def get_balance(self):
+        vals = [i["amount"] for i in self.ledger]
+        balance = sum(vals)
+        return balance
+
+    def transfer(self, amount, other_category):
+        pass
+    
+    def check_funds(self, amount):
+        balance = self.get_balance()
+        if amount > balance:
+            return False
+        return True
 
 
 
 def create_spend_chart(categories):
+    pass
